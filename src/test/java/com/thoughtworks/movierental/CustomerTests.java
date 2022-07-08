a5a5ffca86c54c5d9d650bbe7821f3fba3e00821
pkrishnasamy <pavithra.krishnasamy@sigfig.com>
2022-07-08 16:28:59 +0530

    Added characterization test

--- src/test/java/com/thoughtworks/movierental/CustomerTests.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTests.java
@@ -2,9 +2,23 @@ package com.thoughtworks.movierental;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CustomerTest {
 
     @Test
     public void test() {
+        Customer customer = new Customer("John");
+        customer.addRental(new Rental(new Movie("Movie1", Movie.CHILDRENS), 1));
+        customer.addRental(new Rental(new Movie("Movie1", Movie.NEW_RELEASE), 2));
+        customer.addRental(new Rental(new Movie("Movie1", Movie.REGULAR), 3));
+
+        String expectedStatement = "Rental Record for John\n" +
+                "\tMovie1\t1.5\n" +
+                "\tMovie1\t6.0\n" +
+                "\tMovie1\t3.5\n" +
+                "Amount owed is 11.0\n" +
+                "You earned 4 frequent renter points";
+        assertEquals(expectedStatement, customer.statement());
     }
 }
